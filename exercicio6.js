8d0a6d7a7c41fa6ad61ac2a47640cfde1184be0b
meduardacst5 <dudac555@gmail.com>
2023-09-12 20:59:23 -0300

    que de certo novamente

---
+++ exercicio6.js
@@ -0,0 +1,7 @@
+function exercicio6(){
+    let km = prompt ("quantos km foram percorridos?");
+    let litros = prompt ("quantos litros foram gastos?");
+    let resultado = (km/litros);
+    alert (`seu carro faz ${resultado} km com um L`)
+}
+exercicio6()
